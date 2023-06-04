471bf24ad10ad75a7ede473b21d31030ca9c04a8
penpenpng <poman1638m@gmail.com>
2023-06-05 01:24:49 +0900

    style: indent

--- src/pages/popup/Popup.svelte
+++ src/pages/popup/Popup.svelte
@@ -16,39 +16,39 @@
     value = `${tab.title} ${tab.url}`;
     tabId = tab.id ?? 0;
     chrome.scripting.executeScript({
-    target: { tabId },
-    func: () => {
-      injectResourceScript('js/share-on-nostr.js');
+      target: { tabId },
+      func: () => {
+        injectResourceScript('js/share-on-nostr.js');
 
-      window.addEventListener('message', async ({ data }: MessageEvent<Packet>) => {
-        if (data.ext !== 'share-on-nostr') {
-          return;
+        window.addEventListener('message', async ({ data }: MessageEvent<Packet>) => {
+          if (data.ext !== 'share-on-nostr') {
+            return;
+          }
+
+          if (data.kind === 'relays' || data.kind === 'result') {
+            chrome.runtime.sendMessage(data);
+          }
+        });
+
+        chrome.runtime.onMessage.addListener((packet: Packet) => {
+          if (packet.ext !== 'share-on-nostr') {
+            return;
+          }
+
+          if (packet.kind === 'share') {
+            window.postMessage(packet);
+          }
+        });
+
+        function injectResourceScript(path: string) {
+          const script = document.createElement('script');
+          script.setAttribute('async', 'false');
+          script.setAttribute('type', 'text/javascript');
+          script.setAttribute('src', chrome.runtime.getURL(path));
+          document.head.appendChild(script);
         }
-
-        if (data.kind === 'relays' || data.kind === 'result') {
-          chrome.runtime.sendMessage(data);
-        }
-      });
-
-      chrome.runtime.onMessage.addListener((packet: Packet) => {
-        if (packet.ext !== 'share-on-nostr') {
-          return;
-        }
-
-        if (packet.kind === 'share') {
-          window.postMessage(packet);
-        }
-      });
-
-      function injectResourceScript(path: string) {
-        const script = document.createElement('script');
-        script.setAttribute('async', 'false');
-        script.setAttribute('type', 'text/javascript');
-        script.setAttribute('src', chrome.runtime.getURL(path));
-        document.head.appendChild(script);
-      }
-    },
-  });
+      },
+    });
   });
   chrome.runtime.onMessage.addListener((packet: Packet) => {
     if (packet.ext !== 'share-on-nostr') {
