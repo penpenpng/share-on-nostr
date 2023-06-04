c411265095680fd4dcadbaf0283a062f753fa783
penpenpng <poman1638m@gmail.com>
2023-06-04 22:04:37 +0900

    refactor: move html

--- src/manifest.ts
+++ src/manifest.ts
@@ -12,7 +12,7 @@ export default defineManifest({
     '128': 'img/logo-128.png',
   },
   action: {
-    default_popup: 'popup.html',
+    default_popup: 'src/pages/popup/index.html',
     default_icon: 'img/logo-48.png',
   },
   background: {
