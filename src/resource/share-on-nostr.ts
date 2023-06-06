82e877e571603038808dae685b1f2993b55186e5
penpenpng <poman1638m@gmail.com>
2023-06-07 01:12:46 +0900

    fix: typing

--- src/resource/share-on-nostr.ts
+++ src/resource/share-on-nostr.ts
@@ -81,3 +81,26 @@ interface SignedEvent extends UnsignedEvent {
   sig: string;
   pubkey: string;
 }
+
+interface Nip07 {
+  getPublicKey: () => Promise<string>;
+  signEvent: (event: {
+    kind: number;
+    tags: string[][];
+    content: string;
+    created_at: number;
+  }) => Promise<{
+    id: string;
+    sig: string;
+    kind: number;
+    tags: string[][];
+    pubkey: string;
+    content: string;
+    created_at: number;
+  }>;
+  getRelays(): Promise<{ [url: string]: { read: boolean; write: boolean } }>;
+}
+
+interface Window {
+  nostr?: Nip07;
+}
